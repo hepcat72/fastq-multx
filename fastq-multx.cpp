0499f7ef7bdfa9d3e9f03004de3eb3b4b8d0fe53
hepcat72 <hepcat72@gmail.com>
2020-06-09 16:14:52 -0400

    Added option -f, but needs work. I got -f to work with regular files, but the point was to get it to work when the input was provided via process substitution, and that currently causes the error: "gzip: /dev/fd/63: unexpected end of file" and no reads are split. I may abandon this branch.
    Files checked in: .gitignore fastq-lib.cpp fastq-lib.h fastq-multx.cpp

--- fastq-multx.cpp
+++ fastq-multx.cpp
@@ -34,7 +34,7 @@ See "void usage" below for usage.
 #define THFIXFACTOR 20
 #define endstr(e) (e=='e'?"end":e=='b'?"start":"n/a")
 
-const char * VERSION = "1.3";
+const char * VERSION = "1.5";
 #define SVNREV 1
 
 // barcode
@@ -103,6 +103,9 @@ int main (int argc, char **argv) {
 	char c;
 	bool trim = true;
 	int mismatch = 1;
+	int mismatchdual = -1;
+	int mismatchtotal = 1;
+        bool indepmm = false;
 	int distance = 2;
 	int poor_distance = 0;       // count of skipped reads on distance only
 	int quality = 0;
@@ -118,6 +121,8 @@ int main (int argc, char **argv) {
 	char verify='\0';
 	bool noexec = false;
 	const char *group = NULL;
+	char fmt='\0';
+        bool fmtgz = 0;
     bool usefile1 = false;
     int phred = 33;
     double threshfactor = 1;
@@ -126,7 +131,7 @@ int main (int argc, char **argv) {
 	int i;
 	bool omode = false;
 	char *bfil = NULL;
-	while (	(c = getopt (argc, argv, "-DzxnHhbeov:m:B:g:L:l:G:q:d:t:")) != -1) {
+	while (	(c = getopt (argc, argv, "-DzxnHhbeov:m:M:B:g:L:l:G:f:q:d:t:")) != -1) {
 		switch (c) t:{
 		case '\1':
                        	if (omode) {
@@ -155,6 +160,12 @@ int main (int argc, char **argv) {
 		case 'H': bcinheader = 1; usefile1=1; break;
 		case 'e': end = 'e'; break;
 		case 'G': group = optarg; break;
+		case 'f':
+                        if (strlen(optarg)>1) {
+				fprintf(stderr, "Option -f requires a single character argument");
+				exit(1);
+			}
+			fmt = *optarg; break;
 		case 'g':
 			guide = optarg;
 			in[f_n++] = optarg;
@@ -167,6 +178,7 @@ int main (int argc, char **argv) {
 		case 'n': noexec = true; break;
 		case 't': threshfactor = atof(optarg); break;
 		case 'm': mismatch = atoi(optarg); break;
+		case 'M': mismatchdual = atoi(optarg); break;
 		case 'd': distance = atoi(optarg); break;
 		case 'q': quality = atoi(optarg); break;
 		case 'D': ++debug; break;
@@ -184,15 +196,34 @@ int main (int argc, char **argv) {
 		}
 	}
 
+        mismatchtotal = mismatch;
+        if (mismatchdual >= 0) {
+            mismatchtotal += mismatchdual;
+            indepmm = true;
+        }
+
 	if (group && !list) {
-		fprintf(stderr, "Error: -G only works with -l\n");
-		return 1;
+            fprintf(stderr, "Error: -G only works with -l\n");
+            return 1;
 	}
 
-    if ((list && guide) || (list && bfil) || (guide && bfil)) {
+        if (fmt) {
+            if (fmt != 'g' && fmt != 'z' && fmt != 'd' && fmt != 't') {
+		fprintf(stderr, "Error: Format (-f %c) must be one of: ['g','d','z','t'].\n", fmt);
+		return 1;
+            }
+            else if (fmt == 't') {
+                fmtgz = 0;
+            }
+            else {
+                fmtgz = 1;
+            }
+        }
+
+        if ((list && guide) || (list && bfil) || (guide && bfil)) {
             fprintf(stderr, "Error: Only one of -B -g or -l\n");
             return 1;
-    }
+        }
 
 	if (f_n != f_oarg) {
 		fprintf(stderr, "Error: number of input files (%d) must match number of output files following '-o'.\n", f_n);
@@ -209,7 +240,13 @@ int main (int argc, char **argv) {
 	FILE *fin[6];
 	bool gzin[6]; meminit(gzin);
 	for (i = 0; i < f_n; ++i) {
-		fin[i]=gzopen(in[i],"r",&gzin[i]);
+            if (fmt) {
+                fin[i]=gzopen(in[i],"r",fmt);
+                gzin[i] = fmtgz;
+            }
+            else {
+                fin[i]=gzopen(in[i],"r",&gzin[i]);
+            }
 	}
 
 	// set all to null, zero
@@ -697,7 +734,13 @@ int main (int argc, char **argv) {
                 fseek(fin[i],0,0);
             else {
                 pclose(fin[i]);
-                fin[i]=gzopen(in[i],"r",&gzin[i]);
+                if (fmt) {
+                    fin[i]=gzopen(in[i],"r",fmt);
+                    gzin[i] = fmtgz;
+                }
+                else {
+                    fin[i]=gzopen(in[i],"r",&gzin[i]);
+                }
             }
         }
 
@@ -763,7 +806,7 @@ int main (int argc, char **argv) {
         fprintf(stderr, "End used: %s\n", endstr(end));
 
         if (dual && list) {
-            // trim down possiblities to reduce number of open files, and small stub files
+            // trim down possibilities to reduce number of open files, and small stub files
             dend = (dne > dnb) ? 'e' : 'b';
             fprintf(stderr, "Dual-end used: %s\n", endstr(dend));
             int ocnt = bcnt;
@@ -815,10 +858,19 @@ int main (int argc, char **argv) {
 			bc[b].out[i][p-out[i]]='\0';
 			strcat(bc[b].out[i], bc[b].id.s);
 			strcat(bc[b].out[i], p+1);
-			if (!(bc[b].fout[i]=gzopen(bc[b].out[i], "w", &bc[b].gzout[i]))) {
-				fprintf(stderr, "Error opening output file '%s': %s\n",bc[b].out[i], strerror(errno));
-				return 1;
-			}
+                        if (fmt) {
+                            bc[b].gzout[i] = fmtgz;
+                            if (!(bc[b].fout[i]=gzopen(bc[b].out[i], "w", fmt))) {
+                                fprintf(stderr, "Error opening output file '%s': %s\n",bc[b].out[i], strerror(errno));
+                                return 1;
+                            }
+                        }
+                        else {
+                            if (!(bc[b].fout[i]=gzopen(bc[b].out[i], "w", &bc[b].gzout[i]))) {
+                                fprintf(stderr, "Error opening output file '%s': %s\n",bc[b].out[i], strerror(errno));
+                                return 1;
+                            }
+                        }
 		}
 	}
 
@@ -828,7 +880,13 @@ int main (int argc, char **argv) {
 			fseek(fin[i],0,0);
 		else {
 			pclose(fin[i]);
-			fin[i]=gzopen(in[i],"r",&gzin[i]);
+                        if (fmt) {
+                            fin[i]=gzopen(in[i],"r",fmt);
+                            gzin[i] = fmtgz;
+                        }
+                        else {
+                            fin[i]=gzopen(in[i],"r",&gzin[i]);
+                        }
 		}
 	}
 
@@ -874,7 +932,7 @@ int main (int argc, char **argv) {
 		++nrec;
 		if (read_ok < 0) continue;
 
-		int i, best=-1, bestmm=mismatch+distance+1, bestd=mismatch+distance+1, next_best=mismatch+distance*2+1;
+		int i, best=-1, bestmm=mismatchtotal+distance+1, bestd=mismatchtotal+distance+1, next_best=mismatchtotal+distance*2+1;
 
         if (bcinheader) {
             for (i=f_n-1;i>=0;--i) {
@@ -909,6 +967,7 @@ int main (int argc, char **argv) {
         // for each barcode
         for (i =0; i < bcnt; ++i) {
             int d;
+            int dd = 0;
             if (end == 'e') {
                 if (bc[i].shifted) {
                     if (fq[0].seq.n > bc[i].seq.n) {
@@ -926,10 +985,18 @@ int main (int argc, char **argv) {
 
                 if (dual) {
                     // distance is added in for duals
-                    if (fq[1].seq.n >= bc[i].dual_n) {
-                        d+=hd(fq[1].seq.s+fq[1].seq.n-bc[i].dual_n, bc[i].dual, bc[i].dual_n);
+                    if (indepmm) {
+                        if (fq[1].seq.n >= bc[i].dual_n) {
+                            dd+=hd(fq[1].seq.s+fq[1].seq.n-bc[i].dual_n, bc[i].dual, bc[i].dual_n);
+                        } else {
+                            dd+=bc[i].dual_n;
+                        }
                     } else {
-                        d+=bc[i].dual_n;
+                        if (fq[1].seq.n >= bc[i].dual_n) {
+                            d+=hd(fq[1].seq.s+fq[1].seq.n-bc[i].dual_n, bc[i].dual, bc[i].dual_n);
+                        } else {
+                            d+=bc[i].dual_n;
+                        }
                     }
                 }
             } else {
@@ -939,8 +1006,12 @@ int main (int argc, char **argv) {
                     d=hd(fq[0].seq.s,bc[i].seq.s, bc[i].seq.n);
 
                 // distance is added in for duals
-                if (dual)
-                    d+=hd(fq[1].seq.s,bc[i].dual, bc[i].dual_n);
+                if (dual) {
+                    if (indepmm)
+                        dd+=hd(fq[1].seq.s,bc[i].dual, bc[i].dual_n);
+                    else
+                        d+=hd(fq[1].seq.s,bc[i].dual, bc[i].dual_n);
+                }
 
                 //				if (debug > 1) {
                 //					fprintf(stderr, "index: %d dist: %d bc:%s n:%d", i, d, bc[i].seq.s, bc[i].seq.n);
@@ -949,22 +1020,22 @@ int main (int argc, char **argv) {
                 //				}
             }
             // simple...
-            if (d < bestd) {
+            if ((d + dd) < bestd) {
                 next_best=bestd;
-                bestd=d;
+                bestd=d+dd;
                 if (debug > 1) fprintf(stderr,"next_dist: %d, best_seq: %s:%d\n", next_best, bc[i].seq.s, bestd);
             }
             // if exact match
-            if (d==0) {
+            if ((d+dd)==0) {
                 if (debug) fprintf(stderr, ", found bc: %d bc:%s n:%d, bestd: %d, next_best: %d", i, bc[i].seq.s, bc[i].seq.n, bestd, next_best);
                 best=i;
                 break;
-            } else if (d <= mismatch) {
+            } else if ((d+dd) <= mismatchtotal && (!indepmm || (d <= mismatch && dd <= mismatchdual))) {
                 // if ok match
-                if (d == bestmm) {
+                if ((d+dd) == bestmm) {
                     best=-1;		// more than 1 match... bad
-                } else if (d < bestmm) {
-                    bestmm=d;		// best match...ok
+                } else if ((d+dd) < bestmm) {
+                    bestmm=d+dd;		// best match...ok
                     best=i;
                 }
             }
@@ -1113,17 +1184,19 @@ void pickbest(const void *nodep, const VISIT which, const int depth)
 
 void usage(FILE *f) {
 	fprintf(f,
-"Usage: fastq-multx [-g|-l|-B] <barcodes.fil> <read1.fq> -o r1.%%.fq [mate.fq -o r2.%%.fq] ...\n"
+"Usage: fastq-multx [-g|-l|-B] <barcodes.fil> <read1.fq> [mate.fq] -o r1.%%.fq [-o r2.%%.fq] ...\n"
 "Version: %s.%d\n"
 "\n"
 "Output files must contain a '%%' sign which is replaced with the barcode id in the barcodes file.\n"
-"Output file can be n/a to discard the corresponding data (use this for the barcode read)\n"
+"Output file can be 'n/a' to discard the corresponding data (use this for the index read)\n"
 "\n"
-"Barcodes file (-B) looks like this [where dual index is supported but optional]:\n"
+"The barcodes file (-B) looks like this [where '-NNNNNNNN' for a dual index is optional]:\n"
 "\n"
-"<id1> <file1-sequence>[-<file2-sequence>]\n"
-"<id2> <file1-sequence>[-<file2-sequence>]\n"
+"sample1 ATGGTCCA-TTGAGGAC\n"
+"sample2 GCCTAAGT-AAGCGTCA\n"
 "...\n"
+"\n"
+"The column delimiter may be a space or a tab.  The best matches in the index file(s) must be unique to a single sample's barcode(s) or the read(s) will be considered unmatched.\n"
 "\n"
 "Default is to guess the -bol or -eol based on clear stats.\n"
 "\n"
@@ -1131,11 +1204,11 @@ void usage(FILE *f) {
 "\n"
 "If -l is used then all barcodes in the file are tried, and the *group* with the *most* matches is chosen.\n"
 "\n"
-"Grouped barcodes file (-l or -L) looks like this:\n"
+"Grouped barcodes file (-l or -L) looks like this [where '-NNNNNNNN' for a dual index is optional]:\n"
 "\n"
-"<id1> <sequence1> <group1>\n"
-"<id1> <sequence1> <group1>\n"
-"<id2> <sequence2> <group2>\n"
+"sample1 ATGGTCCA-TTGAGGAC group1\n"
+"sample1 GCCTAAGT-AAGCGTCA group1\n"
+"sample2 CTTAGTTC-CCAAGTAC group2\n"
 "...\n"
 "\n"
 "Mated reads, if supplied, are kept in-sync\n"
@@ -1147,6 +1220,7 @@ void usage(FILE *f) {
 "-l BCFIL    Determine barcodes from any read, using BCFIL as a master list\n"
 "-L BCFIL    Determine barcodes from <read1.fq>, using BCFIL as a master list\n"
 "-B BCFIL    Use barcodes from BCFIL, no determination step, codes in <read1.fq>\n"
+"-f FMT      Force input file format to be one of 'g' for gzip, 'z' for zip, 'd' for dz, or 't' for text. (inferred from file ext)\n"
 "-H          Use barcodes from illumina's header, instead of a read\n"
 "-b          Force beginning of line (5') for barcode matching\n"
 "-e          Force end of line (3') for barcode matching\n"
@@ -1155,7 +1229,8 @@ void usage(FILE *f) {
 "-x          Don't trim barcodes off before writing out destination\n"
 "-n          Don't execute, just print likely barcode list\n"
 "-v C        Verify that mated id's match up to character C (Use ' ' for illumina)\n"
-"-m N        Allow up to N mismatches, as long as they are unique (1)\n"
+"-m N        Allow N mismatches in union of all indexes, unless -M is supplied. (1)\n"
+"-M M        Allow N,M mismatches in indexes 1,2 respectively (see -m N) (not used)\n"
 "-d N        Require a minimum distance of N between the best and next best (2)\n"
 "-q N        Require a minimum phred quality of N to accept a barcode base (0)\n"
 	,VERSION,SVNREV);
